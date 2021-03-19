5f3b721268ec8ba8d9e82e904b7c479901819202
inas-mohammad <inas.hameidan@gmail.com>
2021-03-19 21:28:46 +0200

    exam

---
+++ exam.js
@@ -0,0 +1,130 @@
+ //project 
+ students=[
+{ mohammad 
+    { subject :
+      
+
+    },
+ subject :
+} ;
+{ name : ali }
+{ name : ahmad }
+
+ 
+
+ ]
+ 
+
+
+
+
+// //practice 
+// //Q1
+// const extractStat = (statName, arr) => {
+// const snorlaxData = {
+//     stats: [
+//       {
+//         stat: {
+//           url: "https://pokeapi.co/api/v2/stat/6/",
+//           name: "speed",
+//         },
+//         effort: 5,
+//         baseStat: 30,
+//       },
+//       {
+//         stat: {
+//           url: "https://pokeapi.co/api/v2/stat/5/",
+//           name: "special-defense",
+//         },
+//         effort: 2,
+//         baseStat: 110,
+//       },
+//       {
+//         stat: {
+//           url: "https://pokeapi.co/api/v2/stat/4/",
+//           name: "special-attack",
+//         },
+//         effort: 9,
+//         baseStat: 65,
+//       },
+//     ],
+//     name: "snorlax",
+//     weight: 4600;
+//      for (i=s; i<stats.length;i++){
+//     if (statName === name.stat.stats[i])
+//     { return  stat.stats[i];};
+//         else { return "null";}};
+//   };
+
+
+//   //q2
+//   //a
+//   const characters = [
+//     {
+//       name: "Eddard",
+//       spouse: "Catelyn",
+//       children: ["Robb", "Sansa", "Arya", "Bran", "Rickon"],
+//       house: "Stark",
+//     },
+//     {
+//       name: "Jon",
+//       spouse: "Lysa",
+//       children: ["Robin"],
+//       house: "Arryn",
+//     },
+//     {
+//       name: "Cersei",
+//       spouse: "Robert",
+//       children: ["Joffrey", "Myrcella", "Tommen"],
+//       house: "Lannister",
+//     },
+//     {
+//       name: "Daenarys",
+//       spouse: "Khal Drogo",
+//       children: ["Drogon", "Rhaegal", "Viserion"],
+//       house: "Targaryen",
+//     },
+//     {
+//       name: "Mace",
+//       spouse: "Alerie",
+//       children: ["Margaery", "Loras"],
+//       house: "Tyrell",
+//     },
+//     {
+//       name: "Sansa",
+//       spouse: "Tyrion",
+//       house: "Stark",
+//     },
+//     {
+//       name: "Jon",
+//       spouse: null,
+//       house: "Snow",
+//     },
+//   ];
+//       let count;
+//     ;
+//     const countNumberOfChildren = (arr) => {
+//     for (j=0; j< characters.length;j++)
+//     if (key.characters[i]===key.arr[]){
+//          count++;
+//         }
+//         else { return  count ;}
+//      return count ;
+//   };
+
+
+
+//   //b
+// 1
+
+//   const extractChildren = (arr) => {
+//     for (index=0;index<characters.length;index++){
+ //       charactres.name.split();
+ //            
+
+        
+        
+//     }
+//   };
+
+
